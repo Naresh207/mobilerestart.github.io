72a5c7796979822445a992e262823c49165c1e80
naresh_venkate_official <107973553+Naresh207@users.noreply.github.com>
2023-11-22 19:54:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+  window.location.href = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGxz1dIwIIdelAER-Agsef7pLYAXDXj4PKLyNNypc8DwBcij-nhZSdA3lVcwLXEdx4hhc&usqp=CAU"; // Redirect after the fake restart
+}, 5000); // Change the time (in milliseconds) as needed
